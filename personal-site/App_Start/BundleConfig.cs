3a276d019d6f667abceedb55193e319ef6222438
kyleruss <kyle@kylerussell.co.nz>
2019-03-04 18:21:50 +1300

    Add gitignore and initial project structure

---
+++ personal-site/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace personal_site
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/css/main.css"));
+        }
+    }
+}
---
+++ personal-site/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(personal_site.Startup))]
+namespace personal_site
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
