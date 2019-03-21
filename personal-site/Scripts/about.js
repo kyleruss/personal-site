f750b9d9648ab0867e339f3ab22eb24fb1f50f06
kyleruss <kyle@kylerussell.co.nz>
2019-03-21 16:00:20 +1300

    Add skill carousel scripts

--- personal-site/Scripts/about.js
+++ personal-site/Scripts/about.js
@@ -1,4 +1,45 @@
 ﻿function About()
 {
-    console.log('about loaded');
+    var skills =
+    [
+        "C++",
+        "ANGULAR",
+        "PHP",
+        "CORDOVA",
+        "IONIC",
+        "ANDROID",
+        "JQUERY",
+        "SOCKET.IO",
+        "SQL",
+        "SASS/CSS",
+        "HTML",
+        "BULMA",
+        "BOOTSTRAP",
+        "C#/.NET",
+        "JAVA",
+        "NODE.JS"
+    ];
+
+    function rotateSkills()
+    {
+        var n = skills.length;
+        var i = 0;
+
+        setInterval(function()
+        {
+            var skillText   =   skills[i];
+            var firstSkill  =   $('.skill-display').first()
+            firstSkill.first().remove();
+
+            var skillContainer = $('<div>', { 'class': 'skill-display' })
+            .append($('<h1>').text(skillText));
+
+            $('#skills-container').append(skillContainer);
+
+            i = (i + 1) % n;
+
+        }, 2000);
+    };
+
+    rotateSkills();
 };
