5b4549a901929fbdc0181fc9d18ca1a186510d87
kyleruss <kyle@kylerussell.co.nz>
2019-03-25 19:25:41 +1300

    Add message sending functionality to ContactService

--- personal-site/Services/ContactService.cs
+++ personal-site/Services/ContactService.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 
 namespace personal_site.Services
@@ -11,9 +15,29 @@ namespace personal_site.Services
 
         private ContactService() { }
 
-        public void SendMessage()
+        public void SendMessage(string contactName, string contactEmail, string contactMessage)
         {
+            NameValueCollection config = ConfigurationManager.AppSettings;
 
+            using(SmtpClient smtpClient = new SmtpClient(config.Get("mailHost"), int.Parse(config.Get("mailPort"))))
+            {
+                smtpClient.Host         =   config.Get("mailHost");
+                smtpClient.Port         =   int.Parse(config.Get("mailPort"));
+                smtpClient.Credentials  =   new NetworkCredential(config.Get("mailAddress"), config.Get("mailPassword"));
+                smtpClient.EnableSsl    =   true;
+
+                MailMessage message = PrepareMessage(contactName, contactEmail, contactMessage, config);
+                smtpClient.Send(message);
+            }
+        
+        }
+
+        private MailMessage PrepareMessage(string contactName, string contactEmail, string message, NameValueCollection config)
+        {
+            string messageBody  = string.Format("Name: {0}\nEmail: {1}\nMessage: \n{3}", contactName, contactEmail, message);
+            string messageTitle = "CONTACT MESSAGE";
+
+            return new MailMessage(config.Get("mailAddress"), config.Get("targetAddress"), messageTitle, messageBody);
         }
 
         public static ContactService GetInstance()
