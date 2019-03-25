11917a63ab79334f04ec2216f9cae08b91dfc9b0
kyleruss <kyle@kylerussell.co.nz>
2019-03-25 17:15:50 +1300

    Add Contact service

---
+++ personal-site/Services/ContactService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace personal_site.Services
+{
+    public class ContactService
+    {
+        private static ContactService _instance;
+
+        private ContactService() { }
+
+        public void SendMessage()
+        {
+
+        }
+
+        public static ContactService GetInstance()
+        {
+            _instance = _instance ?? new ContactService();
+            return _instance;
+        }
+    }
+}
