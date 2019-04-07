ce6faad3d7dcde411832e4833688473dab98a7d8
kyleruss <kyle@kylerussell.co.nz>
2019-04-07 16:23:22 +1200

    Add blog controller

---
+++ personal-site/Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using personal_site.Models;
+using personal_site.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace personal_site.Controllers
+{
+    public class BlogController : Controller
+    {
+        public async Task<JsonResult> GetBlogPosts()
+        {
+            BlogService blogService = BlogService.GetInstance();
+            List<BlogPost> blogList = await blogService.GetBlogList();
+            var blogListJson = JsonConvert.SerializeObject(blogList);
+
+            return Json(blogListJson, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
