990830bc2a3459e52c67d1602779f7e83d8b3e60
kyleruss <kyle@kylerussell.co.nz>
2019-04-07 14:55:43 +1200

    Add blog display modal in blog module view

--- personal-site/Content/scripts/main.js
+++ personal-site/Content/scripts/main.js
@@ -54,12 +54,28 @@ function About()
 };
 function Blog()
 {
+    var currentBlog;
+    var blogList;
+
     $('.blog-post-display').hover((e) =>
     {   
         e.stopImmediatePropagation();
         var innerContainer = $(e.target).parent().find('.blog-post-inner-container');
         innerContainer.toggleClass('blog-hover-effect');
     });
+
+    $('.blog-post-display').click(() =>
+    {
+        var blogModal = $('#blog-modal');
+
+        blogModal.modal();
+    });
+
+    $('#blog-modal-close').click(() =>
+    {
+        $('#blog-modal').modal('hide');
+    });
+
 };
 function Portfolio()
 {
--- personal-site/Scripts/blog.js
+++ personal-site/Scripts/blog.js
@@ -1,9 +1,25 @@
 ﻿function Blog()
 {
+    var currentBlog;
+    var blogList;
+
     $('.blog-post-display').hover((e) =>
     {   
         e.stopImmediatePropagation();
         var innerContainer = $(e.target).parent().find('.blog-post-inner-container');
         innerContainer.toggleClass('blog-hover-effect');
     });
+
+    $('.blog-post-display').click(() =>
+    {
+        var blogModal = $('#blog-modal');
+
+        blogModal.modal();
+    });
+
+    $('#blog-modal-close').click(() =>
+    {
+        $('#blog-modal').modal('hide');
+    });
+
 };
