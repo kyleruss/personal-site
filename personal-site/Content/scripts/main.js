d0d9270f65a72208a87685302f3ff778da92bd7d
kyleruss <kyle@kylerussell.co.nz>
2019-04-08 18:19:57 +1200

    Update blog module script to display the blog post images

--- personal-site/Content/scripts/main.js
+++ personal-site/Content/scripts/main.js
@@ -81,20 +81,37 @@ function Blog()
 
     function initBlogList(page)
     {
+        //Hide the displays and re-display them 
         var blogDisplays = $('.blog-post-display');
         blogDisplays.hide();
 
-        var totalPerPage = 3;
-        var actualLength = blogList.length;
-        var total = currentPage * totalPerPage;
-        var startIndex = (currentPage - 1) * totalPerPage;
-        total = total > actualLength? actualLength : total;
+        //Calculate and init the starting index and iterating total
+        var totalPerPage = 3; //Number of posts to display per page
+        var actualLength = blogList.length; //The real total number of posts
+        var total = currentPage * totalPerPage; //The expected number of posts
+        var startIndex = (currentPage - 1) * totalPerPage; //The index to start iterating at page 1: 0, page 2: 3 etc.
+        total = total > actualLength? actualLength : total; //If the expected total is larger than the actual total use the actual total
 
+        //The blog post background gradient
+        //This style is injected into the elements
+        var gradientColor = "rgba(64, 87, 122, 0.6)";
+        var linearBackgroundStyle = `linear-gradient(${gradientColor}, ${gradientColor})`;
+
+        //Display the blog posts and update their content 
+        //Posts are displayed based on their current page
+        //Up to {totalPerPage} posts shown per page
         for(var elementIndex = 0; startIndex < total; elementIndex++, startIndex++)
         {
             var blogTitle = blogList[startIndex].Title;
+            var blogBackground = blogList[startIndex].PostImage;
             var blogElement = $(blogDisplays[elementIndex]);
             
+            //Inject the background images and linear gradient
+            var displayBackgroundStyle = `${linearBackgroundStyle}, url("${blogBackground}")`;
+            var innerElement = blogElement.find('.blog-post-inner-container');
+            innerElement.css('background', displayBackgroundStyle);
+
+            //Update the content and display the post
             blogElement.find('.blog-post-title').text(blogTitle);
             blogElement.attr('data-blog-index', startIndex);
             blogElement.show();
