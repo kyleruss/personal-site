8e664a48e42dc026dfecff84c8d941b34d8c81f8
kyleruss <kyle@kylerussell.co.nz>
2019-04-15 02:46:17 +1200

    Add external login actions in AccountController

--- personal-site/Controllers/AccountController.cs
+++ personal-site/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using personal_site.Models;
+using personal_site.Helpers;
 
 namespace personal_site.Controllers
 {
@@ -52,7 +53,103 @@ namespace personal_site.Controllers
             }
         }
 
-       
+        // POST: /Account/ExternalLogin
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult ExternalLogin(string provider, string returnUrl)
+        {
+            // Request a redirect to the external login provider
+            return new ChallengeResult(provider, Url.Action("ExternalLoginCallback", "Account", new { ReturnUrl = returnUrl }));
+        }
+
+
+        // GET: /Account/ExternalLoginCallback
+        [AllowAnonymous]
+        public async Task<JsonResult> ExternalLoginCallback(string returnUrl)
+        {
+            var loginInfo = await AuthenticationManager.GetExternalLoginInfoAsync();
+            if (loginInfo == null)
+                return ControllerHelper.JsonActionResponse(true, "You need to login");
+            
+
+            // Sign in the user with this external login provider if the user already has a login
+            var result = await SignInManager.ExternalSignInAsync(loginInfo, isPersistent: false);
+            switch (result)
+            {
+                case SignInStatus.Success:
+                    return ControllerHelper.JsonActionResponse(true, "Successfully signed in");
+                case SignInStatus.LockedOut:
+                    return ControllerHelper.JsonActionResponse(false, "Locked out");
+                case SignInStatus.RequiresVerification:
+                    return await SendCode(returnUrl, false);
+                case SignInStatus.Failure:
+                default:
+                    // If the user does not have an account, then prompt the user to create an account
+                    ViewBag.ReturnUrl = returnUrl;
+                    ViewBag.LoginProvider = loginInfo.Login.LoginProvider;
+                    return ControllerHelper.JsonActionResponse(false, "Need to create an account");//View("ExternalLoginConfirmation", new ExternalLoginConfirmationViewModel { Email = loginInfo.Email });
+            }
+        }
+
+        // GET: /Account/SendCode
+        [AllowAnonymous]
+        public async Task<JsonResult> SendCode(string returnUrl, bool rememberMe)
+        {
+            var userId = await SignInManager.GetVerifiedUserIdAsync();
+            if (userId == null)
+            {
+                return ControllerHelper.JsonActionResponse(false, "[Send code] error");
+            }
+            var userFactors = await UserManager.GetValidTwoFactorProvidersAsync(userId);
+            var factorOptions = userFactors.Select(purpose => new SelectListItem { Text = purpose, Value = purpose }).ToList();
+            return ControllerHelper.JsonActionResponse(true, "[Send code success]"); //View(new SendCodeViewModel { Providers = factorOptions, ReturnUrl = returnUrl, RememberMe = rememberMe });
+        }
+
+        //
+        // POST: /Account/ExternalLoginConfirmation
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<JsonResult> ExternalLoginConfirmation(ExternalLoginConfirmationViewModel model, string returnUrl)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return ControllerHelper.JsonActionResponse(true, "[external login] User is already authenticated");
+            }
+
+            if (ModelState.IsValid)
+            {
+                // Get the information about the user from the external login provider
+                var info = await AuthenticationManager.GetExternalLoginInfoAsync();
+                if (info == null)
+                {
+                    return ControllerHelper.JsonActionResponse(false, "[ExternalLoginConfirm] Login failed");
+                }
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var result = await UserManager.CreateAsync(user);
+                if (result.Succeeded)
+                {
+                    result = await UserManager.AddLoginAsync(user.Id, info.Login);
+                    if (result.Succeeded)
+                    {
+                        await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+                        return ControllerHelper.JsonActionResponse(true, "[External login] User successfully logged in");
+                    }
+                }
+                AddErrors(result);
+            }
+
+            ViewBag.ReturnUrl = returnUrl;
+            return View(model);
+        }
+
+        // GET: /Account/ExternalLoginFailure
+        [AllowAnonymous]
+        public ActionResult ExternalLoginFailure()
+        {
+            return View();
+        }
 
         protected override void Dispose(bool disposing)
         {
