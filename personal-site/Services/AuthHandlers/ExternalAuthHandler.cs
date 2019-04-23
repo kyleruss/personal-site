922b6b5207c634dccebe7e582d33e5a56cdbd5e8
kyleruss <kyle@kylerussell.co.nz>
2019-04-23 18:44:47 +1200

    Add ExternalAuthHandler and TwitterAuthHandler

---
+++ personal-site/Services/AuthHandlers/ExternalAuthHandler.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using personal_site.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace personal_site.Services.AuthHandlers
+{
+    public abstract class ExternalAuthHandler
+    {
+        public abstract Task<ApplicationUser> CreateExternalAccount(ExternalLoginInfo loginInfo, 
+            ApplicationUserManager userManager, IAuthenticationManager authManager = null);
+
+
+        protected async Task<ApplicationUser> SaveUser(ExternalLoginInfo loginInfo, ApplicationUserManager userManager, ApplicationUser user = null)
+        {
+            string provider = loginInfo.Login.LoginProvider;
+
+            if (user == null)
+                user = GetDefaultUser(loginInfo);
+
+            var createResult = await userManager.CreateAsync(user);
+
+            if (createResult.Succeeded)
+            {
+                var loginCreateResult = await userManager.AddLoginAsync(user.Id, loginInfo.Login);
+                return user;
+            }
+
+            else return null;
+        }
+
+        protected ApplicationUser GetDefaultUser(ExternalLoginInfo loginInfo)
+        {
+            string provider = loginInfo.Login.LoginProvider;
+
+            return new ApplicationUser()
+            {
+                UserName = GenerateUsername(loginInfo.Email, provider),
+                Email = loginInfo.Email,
+                DisplayName = loginInfo.DefaultUserName,
+                Provider = provider
+            };
+        }
+
+        protected string GenerateUsername(string email, string provider)
+        {
+            return string.Format("{0}-{1}", provider, email);
+        }
+
+    }
+}
---
+++ personal-site/Services/AuthHandlers/TwitterAuthHandler.cs
@@ -0,0 +1,105 @@
+﻿using LinqToTwitter;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using personal_site.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace personal_site.Services.AuthHandlers
+{
+    public class TwitterAuthHandler : ExternalAuthHandler
+    {
+        public override async Task<ApplicationUser> CreateExternalAccount(ExternalLoginInfo loginInfo, 
+            ApplicationUserManager userManager, IAuthenticationManager authManager = null)
+        {
+            SessionStateCredentialStore credentials = await GetTwitterCredentialStore(authManager);
+            if (credentials == null) return null;
+
+            else
+            {
+                var auth = new AspNetAuthorizer
+                {
+                    CredentialStore = credentials
+                };
+
+                var twitterContext = new TwitterContext(auth);
+
+                var accResponse = await
+                (from acc in twitterContext.Account
+                 where (acc.Type == AccountType.VerifyCredentials) && (acc.IncludeEmail == true)
+                 select acc)
+                    .SingleOrDefaultAsync();
+
+                if (accResponse != null && accResponse.User != null)
+                {
+                    User responseUser = accResponse.User;
+
+                    string userEmail = responseUser.Email;
+                    string provider = loginInfo.Login.LoginProvider;
+
+                    ApplicationUser twitterUser = new ApplicationUser()
+                    {
+                        UserName = GenerateUsername(userEmail, provider),
+                        Email = userEmail,
+                        DisplayName = responseUser.Name,
+                        Provider = provider,
+                        ProfilePicture = responseUser.ProfileImageUrl
+                    };
+
+                    ApplicationUser savedUser = await SaveUser(loginInfo, userManager, twitterUser);
+                    if (savedUser != null)
+                        await SaveAccessTokens(twitterUser, credentials);
+
+                    return savedUser;
+                }
+
+                else return null;
+            }
+        }
+
+        private async Task SaveAccessTokens(ApplicationUser user, SessionStateCredentialStore credentials)
+        {
+            using (ApplicationDbContext dbContext = new ApplicationDbContext())
+            {
+                UserAccessTokens userToken = new UserAccessTokens()
+                {
+                    UserId = user.Id,
+                    AccessToken = credentials.OAuthToken,
+                    AccessTokenSecret = credentials.OAuthTokenSecret
+                };
+
+                dbContext.UserAccessTokens.Add(userToken);
+                await dbContext.SaveChangesAsync();
+            }
+        }
+
+        private async Task<SessionStateCredentialStore> GetTwitterCredentialStore(IAuthenticationManager authManager)
+        {
+            var claims = await authManager.GetExternalIdentityAsync(DefaultAuthenticationTypes.ExternalCookie);
+            if (claims == null) return null;
+
+            else
+            {
+                Claim accessToken = claims.FindFirst(LinqToTwitterAuthenticationProvider.AccessToken);
+                Claim accessTokenSecret = claims.FindFirst(LinqToTwitterAuthenticationProvider.AccessTokenSecret);
+                NameValueCollection config = ConfigurationManager.AppSettings;
+
+                return new SessionStateCredentialStore
+                {
+                    ConsumerKey = config.Get("twitterID"),
+                    ConsumerSecret = config.Get("twitterSecret"),
+                    OAuthToken = accessToken.Value,
+                    OAuthTokenSecret = accessTokenSecret.Value
+                };
+            }
+        }
+    }
+}
