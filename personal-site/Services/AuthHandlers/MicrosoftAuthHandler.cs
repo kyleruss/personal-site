c7eb92ef834a1036368a75e4cd1538b0715474ba
kyleruss <kyle@kylerussell.co.nz>
2019-04-24 20:27:19 +1200

    Add MicrosoftAuthHandler

---
+++ personal-site/Services/AuthHandlers/MicrosoftAuthHandler.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using personal_site.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace personal_site.Services.AuthHandlers
+{
+    public class MicrosoftAuthHandler : ExternalAuthHandler
+    {
+
+        public override Task<ApplicationUser> CreateExternalAccount(ExternalLoginInfo loginInfo, 
+        ApplicationUserManager userManager,IAuthenticationManager authManager = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
