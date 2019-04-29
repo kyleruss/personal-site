4ac063024e7728115eb82a0dbb78579beaf9821b
kyleruss <kyle@kylerussell.co.nz>
2019-04-29 19:31:47 +1200

    Add AdminController and Admin/Login view

---
+++ personal-site/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace personal_site.Controllers
+{
+    public class AdminController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
