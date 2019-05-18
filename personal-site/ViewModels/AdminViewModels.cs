626d887a2b139312c73ed093430a733c6494432b
kyleruss <kyle@kylerussell.co.nz>
2019-05-18 17:48:49 +1200

    Add Rss, social media and repo task view models to AdminViewModels

--- personal-site/ViewModels/AdminViewModels.cs
+++ personal-site/ViewModels/AdminViewModels.cs
@@ -15,7 +15,7 @@ namespace personal_site.ViewModels
         public string Password { get; set; }
     }
 
-    public class AdminBlogEditModel
+    public class AdminBlogEditViewModel
     {
         [Required]
         public string Title { get; set; }
@@ -29,9 +29,50 @@ namespace personal_site.ViewModels
         public int PostId { get; set; }
     }
 
-    public class AdminEntityRemovalModel
+    public class AdminEntityRemovalViewModel
     {
         [Required]
         public int EntityId { get; set; }
+    }
+
+    public class AdminRssChannelViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+
+    public class AdminRssItemViewModel
+    {
+        [Required]
+        public string ItemContent { get; set; }
+    }
+
+    public class AdminSocialMediaViewModel
+    {
+        [Required]
+        public string TwitterLink { get; set; }
+
+        [Required]
+        public string GithubLink { get; set; }
+
+        [Required]
+        public string DribbleLink { get; set; }
+
+        [Required]
+        public string RssLink { get; set; }
+
+        [Required]
+        public string StackOverflowLink { get; set; }
+    }
+
+    public class AdminRepoTaskViewModel
+    {
+        [Required]
+        public int TaskId { get; set; }
     }
 }
