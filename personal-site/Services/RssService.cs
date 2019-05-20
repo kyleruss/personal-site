1c2f7bcbc619342ef70871a738180c513064b8a7
kyleruss <kyle@kylerussell.co.nz>
2019-05-21 01:35:36 +1200

    Add RssService

---
+++ personal-site/Services/RssService.cs
@@ -0,0 +1,36 @@
+﻿using personal_site.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace personal_site.Services
+{
+    public class RssService
+    {
+        private static RssService _instance;
+
+        private RssService() { }
+
+        public void UpdateChannel(AdminRssChannelViewModel model)
+        {
+
+        }
+
+        public void PushUpdate(AdminRssItemViewModel model)
+        {
+
+        }
+
+        public void RemoveItem(int id)
+        {
+
+        }
+
+        public static RssService GetInstance()
+        {
+            _instance = _instance ?? new RssService();
+            return _instance;
+        }
+    }
+}
