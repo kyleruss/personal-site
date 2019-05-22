c050414bc14376d67f7e72777d5d449bba01269e
kyleruss <kyle@kylerussell.co.nz>
2019-05-23 01:09:35 +1200

    Add Dashboard controller in admin area

---
+++ personal-site/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace personal_site.Areas.Admin.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("../Dashboard");
+        }
+    }
+}
