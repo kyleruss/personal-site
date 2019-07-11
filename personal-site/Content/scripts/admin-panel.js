aba91c2e94dd99a245c9c8004ef774202c74d5e1
kyleruss <kyle@kylerussell.co.nz>
2019-07-11 18:28:29 +0930

    Update manage-repos script to display repo controls

--- personal-site/Content/scripts/admin-panel.js
+++ personal-site/Content/scripts/admin-panel.js
@@ -11,6 +11,7 @@ function ManageRepos()
     var repoData = {};
 
     loadRepos();
+    $('#repo-list-body').niceScroll(scrollConfig);
 
     function loadRepos()
     {
@@ -26,11 +27,28 @@ function ManageRepos()
                 createRepoTableCell('name', item, rowElement);
                 createRepoTableCell('description', item, rowElement);
                 createRepoTableCell('languages', item, rowElement);
+                createRepoControls(rowElement);
 
                 repoTableContainer.append(rowElement);
             });
         });
     };
+
+    function createRepoControls(rowElement)
+    {
+        var cellElement = $('<td/>');
+        cellElement.attr('class', 'repo-controls-cell');
+
+        var editControlElement = $('<i/>');
+        editControlElement.attr('class', 'far fa-edit repo-control-btn repo-edit-btn');
+        
+        var removeControlElement = $('<i/>');
+        removeControlElement.attr('class', 'fas fa-times repo-control-btn repo-remove-btn');
+
+        cellElement.append(editControlElement);
+        cellElement.append(removeControlElement);
+        rowElement.append(cellElement);
+    }
 
     function createRepoTableCell(name, item, rowElement)
     {
--- personal-site/Scripts/admin/manage-repos.js
+++ personal-site/Scripts/admin/manage-repos.js
@@ -3,6 +3,7 @@
     var repoData = {};
 
     loadRepos();
+    $('#repo-list-body').niceScroll(scrollConfig);
 
     function loadRepos()
     {
@@ -18,11 +19,28 @@
                 createRepoTableCell('name', item, rowElement);
                 createRepoTableCell('description', item, rowElement);
                 createRepoTableCell('languages', item, rowElement);
+                createRepoControls(rowElement);
 
                 repoTableContainer.append(rowElement);
             });
         });
     };
+
+    function createRepoControls(rowElement)
+    {
+        var cellElement = $('<td/>');
+        cellElement.attr('class', 'repo-controls-cell');
+
+        var editControlElement = $('<i/>');
+        editControlElement.attr('class', 'far fa-edit repo-control-btn repo-edit-btn');
+        
+        var removeControlElement = $('<i/>');
+        removeControlElement.attr('class', 'fas fa-times repo-control-btn repo-remove-btn');
+
+        cellElement.append(editControlElement);
+        cellElement.append(removeControlElement);
+        rowElement.append(cellElement);
+    }
 
     function createRepoTableCell(name, item, rowElement)
     {
