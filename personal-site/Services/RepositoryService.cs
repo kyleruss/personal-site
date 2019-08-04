82d210bbe43bae4d0d109bea0448b7bda8bfaad2
kyleruss <kyle@kylerussell.co.nz>
2019-08-04 19:37:06 +0930

    Update Repository to accept task name on RunTask and remove unused const task ids

--- personal-site/Services/RepositoryService.cs
+++ personal-site/Services/RepositoryService.cs
@@ -17,16 +17,6 @@ namespace personal_site.Services
     {
         private static RepositoryService _instance;
 
-        private const int REPO_LOAD_TASK = 0;
-        private const int COMMITS_LOAD_TASK = 1;
-        private const int CODELINES_LOAD_TASK = 2;
-        private const int LANG_LOAD_TASK = 3;
-        private const int README_LOAD_TASK = 4;
-        private const int DESC_LOAD_TASK = 5;
-        private const int CHECK_DATA_TASK = 6;
-        private const int FILTER_DATA_TASK = 7;
-        private const int FILTER_CONTENT_TASK = 8;
-
         private RepositoryService() { }
 
         public async Task RemoveRepository(string name, HttpServerUtilityBase server)
@@ -96,37 +86,17 @@ namespace personal_site.Services
             return server.MapPath("~/Content/resources/repository-data.json");
         }
 
-        public void RunTask(int taskID)
+        public bool RunTask(string taskName)
         {
-            switch(taskID)
+            List<string> taskList = GetRepositoryTaskList();
+
+            if (!taskList.Contains(taskName))
+                return false;
+            else
             {
-                case REPO_LOAD_TASK:
-                    break;
-
-                case COMMITS_LOAD_TASK:
-                    break;
-
-                case CODELINES_LOAD_TASK:
-                    break;
-
-                case LANG_LOAD_TASK:
-                    break;
-
-                case README_LOAD_TASK:
-                    break;
-
-                case DESC_LOAD_TASK:
-                    break;
-
-                case CHECK_DATA_TASK:
-                    break;
-
-                case FILTER_CONTENT_TASK:
-                    break;
-
-                case FILTER_DATA_TASK:
-                    break;
+                return true;
             }
+
         }
 
         public void UpdateRepositories()
