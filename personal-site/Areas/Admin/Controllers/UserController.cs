28830258b016efb5998efb061508e2d68aaac82e
kyleruss <kyle@kylerussell.co.nz>
2019-08-08 17:09:02 +0930

    Add admin user controller and add User view

---
+++ personal-site/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace personal_site.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("../User");
+        }
+    }
+}
