e8a994e789783d0229417e4e21f4af5fe1bd8372
kyleruss <kyle@kylerussell.co.nz>
2019-08-08 17:15:54 +0930

    Add UserService with method to fetch current user list

---
+++ personal-site/Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using personal_site.Models;
+
+namespace personal_site.Services
+{
+    public class UserService
+    {
+        private static UserService _instance;
+
+        private UserService() { }
+
+        public List<ApplicationUser> GetUserList()
+        {
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+                List<ApplicationUser> userList = context.Users.ToList();
+                return userList;
+            }
+        }
+
+        public static UserService GetInstance()
+        {
+            _instance = _instance ?? new UserService();
+            return _instance;
+        }
+    }
+}
