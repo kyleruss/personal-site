a9e634c80043db9631a20ad12c64a0c6d6cc612f
kyleruss <kyle@kylerussell.co.nz>
2019-08-23 22:00:27 +0930

    Update AccountService to send authentication codes

--- personal-site/Services/AccountService.cs
+++ personal-site/Services/AccountService.cs
@@ -10,6 +10,7 @@ using System.Collections.Specialized;
 using System.Configuration;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Mail;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
@@ -18,9 +19,22 @@ namespace personal_site.Services
 {
     public class AccountService
     {
+        public const string SESSION_CODE_NAME = "sys_authcode";
+
         private static AccountService _instance;
 
         private AccountService() { }
+
+        public async Task<bool> SendAuthCode()
+        {
+            MailService mailService = MailService.GetInstance();
+            string authCode = Guid.NewGuid().ToString();
+
+            HttpContext.Current.Session[SESSION_CODE_NAME] = authCode;
+
+            MailMessage message = mailService.PrepareAuthCodeMessage(authCode);
+            return await mailService.SendMessage(message);
+        }
 
         public ExternalAuthHandler GetExternalAuthHandler(ExternalLoginInfo loginInfo)
         {
