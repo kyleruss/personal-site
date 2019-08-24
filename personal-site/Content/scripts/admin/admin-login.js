5054263ca0baea87e0fc5e8c4b6e586b39b54f3a
kyleruss <kyle@kylerussell.co.nz>
2019-08-24 18:07:19 +0930

    Add admin-login script

---
+++ personal-site/Content/scripts/admin/admin-login.js
@@ -0,0 +1,21 @@
+﻿$(function()
+{
+    function verifyCredentials()
+    {
+        postAjaxForm($('#admin-login-form'), (data) =>
+        {
+            
+        });
+    };
+
+    function verifyAuthCode()
+    {
+
+    };
+
+    $('#admin-login-btn').click(function(e)
+    {
+        e.preventDefault();
+        verifyCredentials();
+    });
+});
