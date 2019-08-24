48fa0766e2cfd8e4637fbc6656bb533cc8491130
kyleruss <kyle@kylerussell.co.nz>
2019-08-24 18:33:10 +0930

    Update admin-login script to asychronously verify credentials and authentication codes

--- personal-site/Content/scripts/admin/admin-login.js
+++ personal-site/Content/scripts/admin/admin-login.js
@@ -4,19 +4,49 @@
     {
         postAjaxForm($('#admin-login-form'), (data) =>
         {
+            console.log(data);
             
+            if(data.ActionSuccess)
+                $('#admin-login-modal').modal('show');
+
+            else
+            { 
+                var alert = $('#login-alert');
+                toggleAlertResponse(data, alert);
+                setTimeout(() => { toggleAlertResponse(data, alert, true)}, 2000);
+            }
         });
     };
 
     function verifyAuthCode()
     {
+        postAjaxForm($('#admin-auth-code-form'), (data) =>
+        {
+            console.log(data);
 
+            if(data.ActionSuccess)
+            {
+                var redirectUrl = data.Data.RedirectUrl;
+                console.log(redirectUrl);
+                window.location.href = redirectUrl;
+            }
+
+            else
+            {
+                var alert = $('#auth-code-alert');
+                toggleAlertResponse(data, alert);
+                setTimeout(() => { toggleAlertResponse(data, alert, true)}, 2000);
+            }
+        });
     };
 
-    $('#admin-login-btn').click(function(e)
+    $('#admin-auth-code-btn').click((e) =>
     {
-        e.preventDefault();
-        $('#admin-login-modal').modal('show');
-        //verifyCredentials();
+        verifyAuthCode();
+    });
+
+    $('#admin-login-btn').click((e) =>
+    {
+        verifyCredentials();
     });
 });
