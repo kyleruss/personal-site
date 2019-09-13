b7ffa0c49901e9dc99fa0b6dd40638d14facd34c
kyleruss <kyle@kylerussell.co.nz>
2019-09-13 23:12:02 +0930

    Update about script with slow typing effect of about text

--- personal-site/Content/scripts/main.js
+++ personal-site/Content/scripts/main.js
@@ -42,7 +42,24 @@ function Home()
 };
 function About()
 {
+    var aboutText = "Hello, I'm Kyle. I enjoy creating solutions for interesting problems";
+    displayAboutText();
 
+    function displayAboutText()
+    {
+        var i = 1;
+        setInterval(() =>
+        {
+            if(i <= aboutText.length)
+            {
+                var aboutStr = aboutText.substring(0, i);
+                $('#about-text').text(aboutStr); 
+                i++;
+            }
+
+            else return;
+        }, 100);
+    };
 };
 function Blog()
 {
--- personal-site/Scripts/about.js
+++ personal-site/Scripts/about.js
@@ -1,4 +1,21 @@
 ﻿function About()
 {
+    var aboutText = "Hello, I'm Kyle. I enjoy creating solutions for interesting problems";
+    displayAboutText();
 
+    function displayAboutText()
+    {
+        var i = 1;
+        setInterval(() =>
+        {
+            if(i <= aboutText.length)
+            {
+                var aboutStr = aboutText.substring(0, i);
+                $('#about-text').text(aboutStr); 
+                i++;
+            }
+
+            else return;
+        }, 100);
+    };
 };
